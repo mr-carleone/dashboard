68054152a024885e687ce59ceb33af12e4a8aa8c
iv.eruslanov <iv.eruslanov@yandex.ru>
2024-05-09 19:09:08 +0300

    add view and router web history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/PageHome.vue')
+  },
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: () => import('@/views/PageAuth.vue')
+  },
+  {
+    path: '/list',
+    name: 'List',
+    component: () => import('@/views/PageList.vue')
+  },
+  {
+    path: '/statistic',
+    name: 'Statistic',
+    component: () => import('@/views/PageStatistic.vue')
+  },
+  {
+    path: '/interview/:id',
+    name: 'Interview',
+    component: () => import('@/views/PageInterview.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue')
-    }
-  ]
+  routes: routes
 })
 
 export default router
--- src/views/PageAuth.vue
+++ src/views/PageAuth.vue
@@ -1,3 +1,3 @@
 <script setup lang="ts"></script>
 
-<template>Some content</template>
+<template>Page Auth</template>
