c3926bbd218f3ffa0aa3ade240a1b09009e85a90
iv.eruslanov <iv.eruslanov@yandex.ru>
2024-05-09 19:22:53 +0300

    add check route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import type { RouteRecordRaw } from 'vue-router'
+import { useUserStore } from '@/stores/user'
+import type { RouteRecordRaw, RouteLocationNormalized, NavigationGuardNext } from 'vue-router'
+
+const checkAuth = (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  const userStore = useUserStore()
+  if (!userStore.userId) {
+    next({ name: 'Auth' })
+  } else {
+    next()
+  }
+}
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/PageHome.vue')
+    component: () => import('@/views/PageHome.vue'),
+    beforeEnter: checkAuth
   },
   {
     path: '/auth',
     name: 'Auth',
-    component: () => import('@/views/PageAuth.vue')
+    component: () => import('@/views/PageAuth.vue'),
+    beforeEnter: checkAuth
   },
   {
     path: '/list',
     name: 'List',
-    component: () => import('@/views/PageList.vue')
+    component: () => import('@/views/PageList.vue'),
+    beforeEnter: checkAuth
   },
   {
     path: '/statistic',
     name: 'Statistic',
-    component: () => import('@/views/PageStatistic.vue')
+    component: () => import('@/views/PageStatistic.vue'),
+    beforeEnter: checkAuth
   },
   {
     path: '/interview/:id',
     name: 'Interview',
-    component: () => import('@/views/PageInterview.vue')
+    component: () => import('@/views/PageInterview.vue'),
+    beforeEnter: checkAuth
   }
 ]
 
